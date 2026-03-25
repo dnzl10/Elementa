8411e67279b3a2c722d2c7947b865c6a58ff552f
Althea Raein Quibral <altheavejerano46@gmail.com>
2026-03-25 16:43:11 +0800

    change

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const path = require('path');
 const QRCode = require('qrcode');
 const fs = require('fs');
 
-// adhshdahjsdg
+// setup
 // ---------------- PROFILE PICS UPLOAD ----------------
 
 // 1️⃣ Setup Storage
